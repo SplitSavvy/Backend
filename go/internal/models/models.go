2064be513f4aaa80bdda1641d1bea432c4a50fbc
whisker <navshukla@gmail.com>
2026-03-10 11:25:11 +0530

    feat: add group and groupmemeber struct

--- go/internal/models/models.go
+++ go/internal/models/models.go
@@ -25,3 +25,21 @@ type UserCredential struct {
 	UpdatedAt    time.Time  `json:"updated_at" db:"updated_at"`
 	DeletedAt    *time.Time `json:"deleted_at,omitempty" db:"deleted_at"`
 }
+
+type Group struct {
+	ID        uuid.UUID  `json:"id" db:"id"`
+	Name      string     `json:"name" db:"name"`
+	CreatedBy uuid.UUID  `json:"created_by" db:"created_by"`
+	IsActive  bool       `json:"is_active" db:"is_active"`
+	CreatedAt time.Time  `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at" db:"updated_at"`
+	DeletedAt *time.Time `json:"deleted_at,omitempty" db:"deleted_at"`
+}
+
+type GroupMember struct {
+	GroupID   uuid.UUID  `json:"group_id" db:"group_id"`
+	UserID    uuid.UUID  `json:"user_id" db:"user_id"`
+	Role      string     `json:"role" db:"role"`
+	AddedOn   time.Time  `json:"added_on" db:"added_on"`
+	RemovedOn *time.Time `json:"removed_on,omitempty" db:"removed_on"`
+}
