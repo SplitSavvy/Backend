d3f287b8a327bc5955f6d7a2e07ca78a3f7a1181
whisker <navshukla@gmail.com>
2026-03-11 23:29:34 +0530

    feat: search for already in group

--- go/internal/users/handler.go
+++ go/internal/users/handler.go
@@ -36,6 +36,7 @@ type UserSearchResult struct {
 	ID       uuid.UUID `json:"id"`
 	Name     string    `json:"name"`
 	Username string    `json:"username"`
+	InGroup  bool      `json:"in_group"`
 }
 
 func (h *Handler) HandleCreateUser(w http.ResponseWriter, r *http.Request) {
@@ -138,6 +139,7 @@ func (h *Handler) HandleCreateUser(w http.ResponseWriter, r *http.Request) {
 
 func (h *Handler) SearchUser(w http.ResponseWriter, r *http.Request) {
 	searchTerm := r.URL.Query().Get("q")
+	groupId := r.URL.Query().Get("group_id")
 	if utf8.RuneCountInString(searchTerm) < 3 {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
@@ -145,17 +147,33 @@ func (h *Handler) SearchUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if groupId == "" {
+		sendJSONError(w, "group_id is required", http.StatusBadRequest)
+		return
+	}
+
 	dbSearchTerm := searchTerm + "%"
 	ctx := context.Background()
 
 	query := `
-		SELECT id, name, username 
-		FROM users 
-		WHERE username LIKE $1 
-		LIMIT 5
-	`
+			SELECT 
+    			u.id AS user_id,
+    			u.name,
+    			u.username,
+    			CASE 
+        			WHEN gm.user_id IS NOT NULL THEN TRUE
+        			ELSE FALSE
+    			END AS in_group
+			FROM users u
+			LEFT JOIN group_members gm 
+    			ON u.id = gm.user_id 
+    			AND gm.group_id = $2
+    			AND gm.removed_on IS NULL
+			WHERE u.username LIKE $1
+			LIMIT 5;
+			`
 
-	rows, err := h.DB.Query(ctx, query, dbSearchTerm)
+	rows, err := h.DB.Query(ctx, query, dbSearchTerm, groupId)
 	if err != nil {
 		sendJSONError(w, "Database error", http.StatusInternalServerError)
 		return
@@ -167,7 +185,7 @@ func (h *Handler) SearchUser(w http.ResponseWriter, r *http.Request) {
 
 	for rows.Next() {
 		var user UserSearchResult
-		err := rows.Scan(&user.ID, &user.Name, &user.Username)
+		err := rows.Scan(&user.ID, &user.Name, &user.Username, &user.InGroup)
 		if err != nil {
 			sendJSONError(w, "Error reading user data", http.StatusInternalServerError)
 			return
